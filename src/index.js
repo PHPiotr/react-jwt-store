e77e0268520dee6b0b5e7234b04195fb784bad25
Matt Crute <matt.crute@lanetix.com>
2015-11-18 10:08:01 -0500

    Should be safe to use when token cookie is not present or invalid

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ import events from 'events'
 
 const EventEmitter = events.EventEmitter
 
+const canWarn = () => console && console.warn && typeof console.warn === 'function'
+
 module.exports = (options) => {
   options = extend({ cookie: 'XSRF-TOKEN' }, options)
 
   let token = cookie.get(options.cookie)
-  let user = decode(token)
+  let user
+
+  if (token) {
+    try {
+      user = decode(token)
+    } catch (e) {
+      user = void 0
+      canWarn() && console.warn(`Invalid JWT: ${token}`)
+    }
+  }
 
   return extend({
     getToken () {
@@ -23,7 +34,7 @@ module.exports = (options) => {
     },
 
     getUserId () {
-      return user.id
+      return user ? user.id : void 0
     }
   }, EventEmitter.prototype)
 }
--- test/index.js
+++ test/index.js
@@ -4,23 +4,47 @@ import assert from 'assert'
 import token from './data/token'
 
 describe('Token Store', () => {
+  beforeEach(() => {
+    // HACK around https://github.com/auth0/jwt-decode/issues/5
+    GLOBAL.window = GLOBAL
+
+    // HACK around cookie monster returning undefined when document isn't there
+    GLOBAL.document = {}
+  })
+
+  afterEach(() => {
+    delete GLOBAL.window
+    delete GLOBAL.document
+  })
+
+  describe('sad path', () => {
+    it('should not blow up when cookie is not present', () => {
+      let tokenStore
+      assert.doesNotThrow(() => tokenStore = require('../src')())
+      assert.ok(tokenStore.getToken() === void 0)
+      assert.ok(tokenStore.getUser() === void 0)
+      assert.ok(tokenStore.getUserId() === void 0)
+    })
+
+    it('should not blow up when cookie is invalid', () => {
+      const token = 'g4rBaG3'
+      require('cookie-monster').set('XSRF-TOKEN', token)
+
+      let tokenStore
+      assert.doesNotThrow(() => tokenStore = require('../src')())
+      assert.ok(tokenStore.getToken() === token)
+      assert.ok(tokenStore.getUser() === void 0)
+      assert.ok(tokenStore.getUserId() === void 0)
+    })
+  })
+
   describe('default cookie key', () => {
     let tokenStore
 
     beforeEach(() => {
-      // HACK around https://github.com/auth0/jwt-decode/issues/5
-      GLOBAL.window = GLOBAL
-
-      // HACK around cookie monster returning undefined when document isn't there
-      GLOBAL.document = {}
       require('cookie-monster').set('XSRF-TOKEN', token)
 
       tokenStore = require('../src')()
-    })
-
-    afterEach(() => {
-      delete GLOBAL.window
-      delete GLOBAL.document
     })
 
     it('should get the XSRF-TOKEN and return it', () => {
@@ -49,19 +73,9 @@ describe('Token Store', () => {
     let tokenStore
 
     beforeEach(() => {
-      // HACK around https://github.com/auth0/jwt-decode/issues/5
-      GLOBAL.window = GLOBAL
-
-      // HACK around cookie monster returning undefined when document isn't there
-      GLOBAL.document = {}
       require('cookie-monster').set('NOT-XSRF-TOKEN', token)
 
       tokenStore = require('../src')({ cookie: 'NOT-XSRF-TOKEN' })
-    })
-
-    afterEach(() => {
-      delete GLOBAL.window
-      delete GLOBAL.document
     })
 
     it('should get the NOT-XSRF-TOKEN and return it', () => {
